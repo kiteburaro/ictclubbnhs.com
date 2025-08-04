21f96b0422eedc6efc985aec9d11dbc800ae83f3
kiteburaro <kite.buraro@gmail.com>
2025-08-04 21:43:12 +0800

    Delete assets directory

--- assets/js/index2.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu(){
-    const icon = document.querySelector(".hamburger-icon");
-    const menu = document.querySelector(".div-hamburgerMenu-link");
-    icon.classList.toggle("open");
-    menu.classList.toggle("open");
-}
